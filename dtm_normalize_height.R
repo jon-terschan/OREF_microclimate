8b16da1c87bdd84481a734ac53e87e61ea3d886f
Jonathan Terschanski <j.terschanski@posteo.net>
2023-08-17 16:38:16 +0200

    normalize height batch processing script

---
+++ dtm_normalize_height.R
@@ -0,0 +1,72 @@
+######################################################
+################ DEPENDENCIES ########################
+######################################################
+library(lidR) #to handle Lidar data
+library(RCSF) # for CSF based ground classif
+library(terra) # for rasterization operations
+library(dplyr)# for walk2
+library(purrr)
+library(fs) # for dir_ls, i think superior to list files
+
+######################################################
+################ FUNCTION SETTINGS ###################
+######################################################
+input.filepaths <- dir_ls("D:/OREF_tls_microclimate_project/point_cloud_data/las_files/las_local_coord/clipped_classif", glob = '*.las')
+filenames <- path_file(input.filepaths)
+filenames <- gsub('.{0,14}$', '', filenames)
+output.filepath <- "D:/OREF_tls_microclimate_project/point_cloud_data/las_files/las_local_coord/normalized/"
+dtm.res = 1 
+
+######################################################
+#################### NORMALIZE LAS  ######ca. 30 mins#
+######################################################
+normalizeLAS <- function(input, output, method) {
+  if(method == "hybrid") {
+    las <- readLAS(input, select = "xyzrnc")
+    dtm_tin <- rasterize_terrain(las, res = dtm.res, algorithm = tin())
+    message("Normalization complete. Exporting...")
+    nlas <- normalize_height(las, tin(), dtm = dtm_tin)
+    writeLAS(nlas, 
+           paste0(output.filepath, output,  "_normalized_", method, ".las"))
+    message("Export complete.")
+  }
+  if(method == "dtm") {
+    las <- readLAS(input, select = "xyzrnc")
+    dtm_tin <- rasterize_terrain(las, res = dtm.res, algorithm = tin())
+    nlas <- las - dtm_tin
+    message("Normalization complete. Exporting...")
+    writeLAS(nlas, 
+             paste0(output.filepath, output,  "_normalized_", method, ".las"))
+    message("Export complete.")
+  }
+  if(method == "tin") {
+    las <- readLAS(input, select = "xyzrnc")
+    dtm_tin <- rasterize_terrain(las, res = dtm.res, algorithm = tin())
+    nlas <- normalize_height(las, tin())
+    message("Normalization complete. Exporting...")
+    writeLAS(nlas, 
+             paste0(output.filepath, output,  "_normalized_", method, ".las"))
+    print(paste0(" Bing Bong! ", output, " is done!"))
+    message("Export complete.")
+  }
+  if(method == "knnidw") {
+    las <- readLAS(input, select = "xyzrnc")
+    dtm_tin <- rasterize_terrain(las, res = dtm.res, algorithm = tin())
+    nlas <- normalize_height(las, knnidw())
+    message("Normalization complete. Exporting...")
+    writeLAS(nlas, 
+             paste0(output.filepath, output,  "_normalized_", method, ".las"))
+    message("Export complete.")
+  }
+}
+
+######################################################
+#################### EXECUTE FUNCTION ####ca. 30 mins#
+######################################################
+# Method must be defined as string. 
+# Valid methods: dtm, tin, knnidw, hybrid 
+# (see https://r-lidar.github.io/lidRbook/norm.html")
+# note that execution time ramps up if there is no DTM involved
+# hybrid and DTM take roughly 30 mins, the others should take
+# MUCH longer, like maybe half a day?
+walk2(input.filepaths, filenames, method = "tin", normalizeLAS)
