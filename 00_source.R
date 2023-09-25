144dc088bb79a124d9484b56674f7f2edbaa6fbd
Jonathan Terschanski <j.terschanski@posteo.net>
2023-09-25 15:17:08 +0200

    added further comments

--- 00_source.R
+++ 00_source.R
@@ -1,7 +1,7 @@
 ######################################################
 #############EDYSAN TLS PROCESSING PIPELINE ##########
 ######################################################
-# CHECK README.MD FOR FAQ AND OVERVIEW OVER THE PIPELINE
+# CHECK README.MD FOR OVERVIEW AND FAQ
 # Scripts can be executed independently if dependencies and custom functions 
 # are loaded in.
 
@@ -11,17 +11,17 @@
 library(lidR) # to handle Lidar data
 library(RCSF) # for CSF based ground classif
 library(terra) # for rasterization operations
-library(raster) # to EXPORT DTM
-library(dplyr) # for walk2
-library(purrr) # for walk2
+library(raster) # to export DTMs, CHMs, etc.
+library(dplyr) # for walk2/data manipulation functions
+library(purrr) # for walk2/map functions
 library(fs) # for directory management functions
 library(here) # for operating system agnostic working directory
 library(ggplot2) # for plotting models
-# library(remotes)
+# library(remotes) # to install packages remotely.
 # remotes::install_github("Molina-Valero/FORTLS", ref = "devel", dependencies = TRUE)
 library(FORTLS) # for forest inventory  
 # remotes::install_github('tiagodc/TreeLS')
-library(TreeLS)# for forest inventory  
+# library(TreeLS)# for forest inventory  
 library(VoxR) # for voxelization
 library(less) # for KDTree nearest neighbor implementation
 library(tidyverse) # load and manipulate data
@@ -30,8 +30,10 @@ library(plotly) # interactive plots
 ######################################################
 ################ SOURCE SCRIPTS ######################
 ######################################################
-# SOURCE THESE SEQUENTIALLY
+# SOURC CUSTOM FUNCTIONS
 source( here::here("functions.R") ) # 0 seconds
+
+# SOURCE THESE SEQUENTIALLY
 source( here::here("01_create_dirs.R") ) # 0 seconds
 source( here::here("02_clip_classif.R") ) # 20 mins
 
